6247631c561267f66485e9efecf9448f1dd54137
Eric King <e.king117@gmail.com>
2016-01-28 22:27:07 -0500

    Added 1/10 second counting, currently broken

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function(){
-  var time1 = {time: 240, active: true};
-  var time2 = {time: 240, active: false};
+  var time1 = {time: 240.0, active: true};
+  var time2 = {time: 240.0, active: false};
   var timer1 = document.getElementById('timerOne');
   var timer2 = document.getElementById('timerTwo');
   document.getElementById('timerOneButton').addEventListener("click", clickOne, false);
@@ -9,9 +9,9 @@
 
   function setTimer(){
     if (time1.active){
-      timer1.textContent = --time1.time;
+      timer1.textContent = time1.time - 0.1;
     } else if (time2.active){
-      timer2.textContent = --time2.time;
+      timer2.textContent = time2.time - 0.1;
     }
   }
 
@@ -26,13 +26,17 @@
   }
 
   function reset(){
-    time1 = {time: 240, active: true};
-    time2 = {time: 240, active: false};
+    time1 = {time: 240.0, active: true};
+    time2 = {time: 240.0, active: false};
     timer1.textContent = time1.time;
     timer2.textContent = time2.time;
   }
 
   setInterval(function(){
     setTimer();
-  }, 1000);
+  }, 100);
 })();
+
+var test = function(arg){
+  console.log(arg.textContent);
+};
